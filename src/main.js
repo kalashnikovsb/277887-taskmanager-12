28fd2f8920c6fe4b870759a179e5e55e3272ab82
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-08-06 00:46:06 +0700

    Разобрал коммит 3.6

--- src/main.js
+++ src/main.js
@@ -1,29 +1,33 @@
-const TASKS_COUNT = 3;
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
 import {createSiteMenuTemplate} from "./view/createSiteMenuTemplate.js";
 import {createFilterTemplate} from "./view/createFilterTemplate.js";
 import {createBoardTemplate} from "./view/createBoardTemplate.js";
 import {createTaskEditTemplate} from "./view/createTaskEditTemplate.js";
 import {createTaskTemplate} from "./view/createTaskTemplate.js";
 import {createLoadMoreButtonTemplate} from "./view/createLoadMoreButtonTemplate.js";
+import {generateTask} from "./mock/task.js";
+
+const TASKS_COUNT = 3;
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
 const boardElement = siteMainElement.querySelector(`.board`);
-const tasksListElement = siteMainElement.querySelector(`.board__tasks`);
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
-render(tasksListElement, createTaskEditTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
 for (let i = 0; i < TASKS_COUNT; i++) {
-  render(tasksListElement, createTaskTemplate(), `beforeend`);
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
---
+++ src/mock/task.js
@@ -0,0 +1,81 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const generateDescription = () => {
+  const descriptions = [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`
+  ];
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+  return descriptions[randomIndex];
+};
+
+const generateDate = () => {
+  // Когда в руках молоток, любая проблема - гвоздь.
+  // Вот и для генерации случайного булевого значения
+  // можно использовать "функцию из интернета".
+  // Ноль - ложь, один - истина. Для верности приводим
+  // к булевому типу с помощью Boolean
+  const isDate = Boolean(getRandomInteger(0, 1));
+  if (!isDate) {
+    return null;
+  }
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+  // По заданию дедлайн у задачи устанавливается без учёта времеми,
+  // но объект даты без времени завести нельзя,
+  // поэтому будем считать срок у всех задач -
+  // это 23:59:59 установленной даты
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+  return new Date(currentDate);
+};
+
+const generateRepeating = () => {
+  return {
+    mo: false,
+    tu: false,
+    we: Boolean(getRandomInteger(0, 1)),
+    th: false,
+    fr: Boolean(getRandomInteger(0, 1)),
+    sa: false,
+    su: false
+  };
+};
+
+const getRandomColor = () => {
+  const colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+  const randomIndex = getRandomInteger(0, colors.length - 1);
+  return colors[randomIndex];
+};
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+  const repeating = dueDate === null
+    ? generateRepeating()
+    : {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    };
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating,
+    color: getRandomColor(),
+    isArchive: Boolean(getRandomInteger(0, 1)),
+    isFavorite: Boolean(getRandomInteger(0, 1)),
+  };
+};
